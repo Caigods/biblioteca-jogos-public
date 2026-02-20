0d057c2b1b9ef57ce1d2f8aeaed2decdbd5320a0
Caigods <caique.godinho337@outlook.com>
2026-02-20 00:58:02 -0300

    Service Completed, building Controller

---
+++ src/main/java/com/caigods/biblioteca_jogos/infrasctuture/entity/enums/PlataformaJogo.java
@@ -0,0 +1,60 @@
+package com.caigods.biblioteca_jogos.infrasctuture.entity.enums;
+
+public enum PlataformaJogo {
+    PC,
+
+    //CONSOLES
+    // PRIMEIRA E SEGUNDA GERAÇÃO
+    MAGNAVOX_ODYSSEY,
+    ATARI_2600,
+    COLECOVISION,
+    INTELLIVISION,
+
+    // TERCEIRA GERAÇÃO
+    NES,
+    MASTER_SYSTEM,
+    ATARI_7800,
+
+    // QUARTA GERAÇÃO
+    SNES,
+    MEGA_DRIVE,
+    NEO_GEO,
+    TURBOGRAFX_16,
+
+    // QUINTA GERAÇÃO
+    PLAYSTATION_1,
+    NINTENDO_64,
+    SEGA_SATURN,
+    IM_3D0,
+
+    // SEXTA GERAÇÃO
+    PLAYSTATION_2,
+    DREAMCAST,
+    GAMECUBE,
+    XBOX,
+
+    // SÉTIMA GERAÇÃO
+    PLAYSTATION_3,
+    XBOX_360,
+    WII,
+
+    // OITAVA GERAÇÃO
+    PLAYSTATION_4,
+    XBOX_ONE,
+    WII_U,
+    NINTENDO_SWITCH,
+
+    // NONA GERAÇÃO
+    PLAYSTATION_5,
+    XBOX_SERIES_X,
+    XBOX_SERIES_S,
+
+    // PORTÁTEIS
+    GAME_BOY,
+    GAME_GEAR,
+    PSP,
+    NINTENDO_DS,
+    PS_VITA,
+    NINTENDO_3DS
+
+}
---
+++ src/test/java/com/caigods/biblioteca_jogos/business/JogoServiceTest.java
@@ -0,0 +1,13 @@
+package com.caigods.biblioteca_jogos.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JogoServiceTest {
+
+    @Test
+    void adicionarHorasPorID() {
+
+    }
+}
