fe6531ed148dd6c866cff5284f7353bc5186729b
Caigods <caique.godinho337@outlook.com>
2026-03-09 17:17:08 -0300

    added DTO's to POST and PUT

---
+++ src/main/java/com/caigods/biblioteca_jogos/exception/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.caigods.biblioteca_jogos.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable throwable) {
+        super(message);
+    }
+}
