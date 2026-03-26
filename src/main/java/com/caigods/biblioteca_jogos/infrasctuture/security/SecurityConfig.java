746228ffe64274b43d1a688f8d4964dc7b8a6d42
Caigods <caique.godinho337@outlook.com>
2026-03-26 00:08:10 -0300

    Config cors

--- src/main/java/com/caigods/biblioteca_jogos/infrasctuture/security/SecurityConfig.java
+++ src/main/java/com/caigods/biblioteca_jogos/infrasctuture/security/SecurityConfig.java
@@ -83,6 +83,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:5174"));
+        config.setAllowedOrigins(List.of("https://*.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
